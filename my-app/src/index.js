7ddb90614775be4885ba68f958cfd4026fa4fb53
onib0002 <onib0002@algonquinlive.com>
2021-11-05 15:47:21 -0400

    root Hash router added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {HashRouter} from 'react-router-dom';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter hashType ="hashbang">
+       <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
