a6e99b9c42e215c94603c701dc451398a112168a
onib0002 <onib0002@algonquinlive.com>
2021-11-06 13:48:53 -0400

    Hash router Added to JS file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {HashRouter} from 'react-router-dom';
-import App from './components/App/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
